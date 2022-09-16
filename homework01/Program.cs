bc6edfe2a57872a0d1d07b44df992d123df67d63
*Svetlana <svetanobrasil@gmail.com>
2022-09-16 19:57:02 +0300

    add2

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = 4;
+
+if (number % 2==0)
+{
+Console.WriteLine("ДА");
+}
+else
+{   
+Console.WriteLine("НЕТ");
+}
