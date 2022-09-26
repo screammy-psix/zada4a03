bd359c2b175987c6a30ef15a17de1ae0e55af455
*Svetlana <svetanobrasil@gmail.com>
2022-09-26 20:01:52 +0300

    ничего не понятно

---
+++ lesson03/example02/123/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[]array);
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+void SelectionSort(int[]array)
+{
+    for (int i=0; i < array.Length; i++);
+    {
+        int minPosition=i;
+        for (int j = int+1; j < array.Lenght; j++)
+        {
+            if(array[j] < array[minPosition] 
+            {
+                minPosition = j;
+            }
+        }
+    int temporary = array[i];
+    array[i]=array[minPosition];
+    array[minPosition] = temporary;
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
---
+++ lesson03/example02/Program.cs
@@ -0,0 +1,48 @@
+﻿//string res = Method4(10, "пых");
+
+for (int i = 2; i <=10; i++)
+{
+    for(int j = 2; j <=10; j++)
+    {
+        Console.WriteLine($"{i} * {j}");
+        
+    }
+    Console.WriteLine();
+}
+
+string text = " - я думаю, сказал князь, улыбаясь, - что, "
+
+                + "ежели бы вас послали вместо еашего милого Винценгороде,"
+                + "вы бы взяли приступом согласие прусского короля."
+                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+//string s = "qwerty"
+//            012
+//s[3//r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i]== oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace (text, ' ' , '|' );
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace (newText,'к','К' );
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace (newText,'С','с' );
+Console.WriteLine(newText);
+
